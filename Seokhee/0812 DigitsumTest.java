89b245578745b0bfb20f72b1103c88f807a93568
CSH <51379713+tjrgmlwlghks@users.noreply.github.com>
2020-08-12 15:17:48 +0900

    2주차 재업로드

--- Seokhee/0812 DigitsumTest.java
+++ Seokhee/0812 DigitsumTest.java
@@ -17,12 +17,13 @@ public class DigitsumTest {
 		
 	}
 	static int sum = 0;
-	static void stt(String st) {
+	//재귀
+	static void stt(String st) { //길이만큼 반복으로 자릿수를 다 더해준다
 		int len = st.length();
-		for(int i=0;i<len;i++) {
+		for(int i=0;i<len;i++) { 
 			sum += st.charAt(i)-'0';
 		}
-		if(sum>=10) {
+		if(sum>=10) { //그 결과가 10이상일 경우 한번더 반복
 			String st2 = Integer.toString(sum);
 			sum = 0;
 			stt(st2);
